65d0651b774d0f5918cc5bdf49e31bb676157fd4
JJunio <junio.araujo01@yahoo.com>
2021-07-09 00:34:45 -0300

    Incluindo recursos com JS

---
+++ scripts/detalhamento.js
@@ -0,0 +1,16 @@
+meuLink = document.getElementsByClassName('link-lista')
+
+for (let index = 0; index < meuLink.length; index++) {
+    meuLink[index].addEventListener('mouseover', alterarCor)
+    meuLink[index].addEventListener('mouseout', retornarCor)
+}
+
+function alterarCor() {
+    this.style.color = '#ffbf00';
+    this.style.fontSize = '22px';
+}
+
+function retornarCor() {
+    this.style.color = 'whitesmoke';
+    this.style.fontSize = '16px';
+}
