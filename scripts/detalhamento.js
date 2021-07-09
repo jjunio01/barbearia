ce8ce835ff8ed323c4a52f7800630a30aa18ef74
JJunio <junio.araujo01@yahoo.com>
2021-07-09 00:51:57 -0300

    Alteração da cor do menu

--- scripts/detalhamento.js
+++ scripts/detalhamento.js
@@ -6,7 +6,7 @@ for (let index = 0; index < meuLink.length; index++) {
 }
 
 function alterarCor() {
-    this.style.color = 'red';
+    this.style.color = '#ffbf00';
     this.style.fontSize = '22px';
 }
 
